c203e4292f1f6fca5ea66da59d8dba991637ec77
vibek lamichhane <vibek@copenned.com>
2022-12-18 10:45:59 +0545

    react drag and drop same and multiple lists

--- src/App.js
+++ src/App.js
@@ -1,23 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { DragDropContext,Droppable,Draggable } from 'react-beautiful-dnd';
+import { useState } from 'react';
+const list = [
+  {
+    id:"0",
+    name:"Bibek",
+    age:"20"
+  },
+  {
+    id:"1",
+    name:"Vibek",
+    age:"21"
+  },
+  {
+    id:"2",
+    name:"Bidhya",
+    age:"23"
+  },
+  {
+    id:"3",
+    name:"Bijaya",
+    age:"18"
+  },
+  {
+    id:"4",
+    name:"Radhika",
+    age:"49"
+  },
+  {
+    id:"5",
+    name:"Mani ram",
+    age:"54"
+  },
+  {
+    id:"6",
+    name:"vibs",
+    age:"21"
+  },
+  {
+    id:"7",
+    name:"Bimal",
+    age:"24"
+  },
+  {
+    id:"8",
+    name:"Sanjeev",
+    age:"24"
+  },
+  {
+    id:"9",
+    name:"G",
+    age:"21"
+  },
+]
 function App() {
+const [items, setItems] = useState(list)
+
+
+
+const handleOnDragEnd = (result)=>{
+  if(!result.destination)return
+  const newItems = Array.from(items)
+  const [recordedItem] = newItems.splice(result.source.index,1) 
+  newItems.splice(result.destination.index,0,recordedItem)
+setItems(newItems)
+}
+// console.log(items)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Just a List</h1>
+      <DragDropContext   onDragEnd={handleOnDragEnd}>
+<Droppable droppableId='drop-1'>
+
+    {provided=>( 
+       <ul className="items"  {...provided.droppableProps} ref={provided.innerRef}>
+        {items.map((l,i)=>{
+          return(
+            <Draggable key={l.id} draggableId={l.id} index={i}>
+              {(provided)=>(
+
+
+            <div key={l.id} className="item" {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef} >
+                {l.name}
+            </div>
+              )}
+            </Draggable>
+          )
+        })}
+        {provided.placeholder}
+      </ul>)}
+        </Droppable>
+        </DragDropContext>
     </div>
   );
 }
